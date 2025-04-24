d0b1017fae910c5c0e9bd06db5865a8c43c96575
SoftDevCLF <cintya.lara.f@gmail.com>
2025-04-24 14:54:51 -0600

    🔨 Create Person Abstract class and modify Customer

---
+++ DataModel/Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookstorePointOfSale.DataModel
+{
+    /// <summary>
+    /// Abstract representation of a person
+    /// </summary>
+    public abstract class Person
+    {
+        /// <summary>
+        /// Property for customer first name
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Property for customer last name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Property for customer email
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Property for customer phone number
+        /// </summary>
+        public string PhoneNumber { get; set; }
+        
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public Person() { }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="email"></param>
+        /// <param name="phoneNumber"></param>
+        public Person( string firstName, string lastName, string email, string phoneNumber)
+        {
+            
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
