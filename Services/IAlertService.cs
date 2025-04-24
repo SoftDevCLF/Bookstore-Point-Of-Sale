e101e336dbad490e296a668fa6ae14f5be5a65ba
SoftDevCLF <cintya.lara.f@gmail.com>
2025-04-23 23:40:24 -0600

    🛠️ Create Services and their Interfaces

---
+++ Services/IAlertService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookstorePointOfSale.Services
+{
+    public interface IAlertService
+    {
+        Task JSAlert(string message);
+        Task<bool> ConfirmAlert(string message);
+    }
+}
---
+++ Services/ICustomerValidation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookstorePointOfSale.Services
+{
+    public interface ICustomerValidation
+    {
+        Task<bool> ValidateName(string firstName, string lastName);
+        Task<bool> ValidatePhoneNumber(string phoneNumber);
+    }
+}
